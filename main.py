7f16afcb37c826023cbcd4e4f6b5f2103bea549c
aleks-oil <aleksoil82@gmail.com>
2024-11-18 19:37:19 +0500

    18/11

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 print('Hi, PyCharm')
-x=43
-y=32
-print(x*y)
+x = 43
+y = 32
+print(x * y)
 print("End line")
--- module_2_2.py
+++ module_2_2.py
@@ -1,9 +1,10 @@
 print('практическое задание "Условная конструкция. Операторы if,elif,else"')
-first = int (input('Введите первое целое число:',))
-second =int (input('Введите второе целое число:',))
-thrid =int (input('Введите третье целое число:',))
-if  first ==second and first==thrid:
+first = int(input('Введите первое целое число:', ))
+second = int(input('Введите второе целое число:', ))
+thrid = int(input('Введите третье целое число:', ))
+if first == second and first == thrid:
     print(3)
-elif first==second or first==thrid or second==thrid:
+elif first == second or first == thrid or second == thrid:
     print(2)
-else: print(0)
+else:
+    print(0)
