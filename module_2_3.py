faec82b359e4f3ee9670f06f2d26801e2572c64f
aleks-oil <aleksoil82@gmail.com>
2024-11-20 20:30:15 +0500

    20/11

--- module_2_3.py
+++ module_2_3.py
@@ -1,8 +1,5 @@
-from operator import index
-from typing import List, Any
-
 print('практическое задание "цикл while"')
-my_list: list[int | Any] = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 a = 0
 while a < len(my_list):
         if my_list[a] > 0:
