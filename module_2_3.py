2b3b7cf86b5dae0eac58c776a6d63dbd6a52a955
aleks-oil <aleksoil82@gmail.com>
2024-11-20 21:10:45 +0500

    20/11_2

--- module_2_3.py
+++ module_2_3.py
@@ -11,3 +11,17 @@ while a < len(my_list):
          a += 1
         elif my_list[a] < 0:
          break
+print('проверка универсальности кода')
+my_list = [42, 69, 322, 13, 0, 99, 0, 1, -5, 9, 8, 7, -6, 5]
+print(my_list, 'код работает при встрече нескольких нулей в списке')
+b = 0
+while b < len(my_list):
+        if my_list[b] > 0:
+         print(my_list[b])
+         b += 1
+         continue
+        elif my_list[b] ==0:
+         pass
+         b += 1
+        elif my_list[b] < 0:
+         break
