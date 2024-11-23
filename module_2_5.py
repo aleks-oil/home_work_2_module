3b5eba68265139e9358643a8d9a56217869289bd
aleks-oil <aleksoil82@gmail.com>
2024-11-23 10:48:06 +0500

    23/11_2й_вариант_решения

--- module_2_5.py
+++ module_2_5.py
@@ -15,3 +15,19 @@ result3 = get_matrix(4, 2, 13)
 print(result1)
 print(result2)
 print(result3)
+
+print('решение с одним циклом')
+def get_matrix (n, m, value):
+    matrix = []
+    for i in range (n):
+        matrix_new = [value]*m
+        matrix.append(matrix_new)
+    return matrix
+
+
+result1 = get_matrix(2, 2, 10)
+result2 = get_matrix(3, 5, 42)
+result3 = get_matrix(4, 2, 13)
+print(result1)
+print(result2)
+print(result3)
